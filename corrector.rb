f9f3dc59fcd1927b7154acb93c0ed9a1a2f7de09
mithlesh kumar <mithlesh.9m@gmail.com>
2022-04-05 18:33:23 +0530

    changes according to reviewer

--- corrector.rb
+++ corrector.rb
@@ -1,5 +1,30 @@
-class Corrector
-  def correct_name(name)
-    name.capitalize.slice(0, 10)
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+class Corrector < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @namable.correct_name
+  end
+end
+
+class CapitalizeCorrector < Corrector
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerCorrector < Corrector
+  def correct_name
+    @nameable.correct_name.slice(0, 10) unless @nameable.correct_name.length <= 10
   end
 end
